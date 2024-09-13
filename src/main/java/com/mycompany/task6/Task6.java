39652de020b0218ca44cf1a59aa91b02cfeb3f6c
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-09-13 16:15:36 +0000

    First commit

---
+++ src/main/java/com/mycompany/task6/Task6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.task6;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Task6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
